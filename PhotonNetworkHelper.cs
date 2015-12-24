80edd92df09e9e87ed5999bf93507ca294cca12c
SnpM <jpthek9@gmail.com>
2015-12-24 12:52:39 -0700

    Stabilize

--- PhotonNetworkHelper.cs
+++ PhotonNetworkHelper.cs
@@ -1,13 +1,23 @@
-﻿using UnityEngine;
+﻿#if false
+using UnityEngine;
 using System.Collections;
-
-namespace Lockstep.Photon
+using Photon;
+namespace Lockstep
 {
+    //TODO: Finish PhotonNetworkHelper
     public class PhotonNetworkHelper : NetworkHelper
     {
+        public override bool IsConnected
+        {
+            get
+            {
+
+            }
+        }
         public override void Disconnect()
         {
             
         }
     }
 }
+#endif
